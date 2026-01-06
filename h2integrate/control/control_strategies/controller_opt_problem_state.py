065f55d8359395802f12891795bf597a5112c7ba
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-01-06 16:51:04 -0700

    refactored DispatchProblemState

--- h2integrate/control/control_strategies/controller_opt_problem_state.py
+++ h2integrate/control/control_strategies/controller_opt_problem_state.py
@@ -19,126 +19,55 @@ class DispatchProblemState:
         self._n_non_optimal_solves = 0
 
     def store_problem_metrics(self, solver_results, start_time, n_days, objective_value):
-        self.start_time = start_time
-        self.n_days = n_days
-        self.termination_condition = str(solver_results.solver.termination_condition)
-        try:
-            self.solve_time = solver_results.solver.time
-        except AttributeError:
-            self.solve_time = solver_results.solver.wallclock_time
-        self.objective = objective_value
-        self.upper_bound = solver_results.problem.upper_bound
-        self.lower_bound = solver_results.problem.lower_bound
-        self.constraints = solver_results.problem.number_of_constraints
-        self.variables = solver_results.problem.number_of_variables
-        self.non_zeros = solver_results.problem.number_of_nonzeros
+        self.value("start_time", start_time)
+        self.value("n_days", n_days)
+
+        solver_results_dict = {
+            k.lower().replace(" ", "_"): v.value
+            for k, v in solver_results.solver._list[0].items()
+            if k != "Statistics"
+        }
+        solver_problem_dict = {
+            k.lower().replace(" ", "_"): v.value for k, v in solver_results.problem._list[0].items()
+        }
+        prob_to_attr_map = {
+            "number_of_nonzeros": "non_zeros",
+            "number_of_variables": "variables",
+            "number_of_constraints": "constraints",
+            "lower_bound": "lower_bound",
+            "upper_bound": "upper_bound",
+        }
+
+        self.termination_condition = str(solver_results_dict["termination_condition"])
+        if "time" in solver_results_dict:
+            self.value("solve_time", solver_results_dict["time"])
+        else:
+            self.value("solve_time", solver_results_dict["wallclock_time"])
+
+        self.value("objective", objective_value)
+
+        for solver_prob_key, attribute_name in prob_to_attr_map.items():
+            self.value(attribute_name, solver_problem_dict[solver_prob_key])
 
         # solver_results.solution.Gap not define
+        upper_bound = solver_problem_dict["upper_bound"]
+        lower_bound = solver_problem_dict["lower_bound"]
         if solver_results.problem.upper_bound != 0.0:
-            self.gap = abs(
-                solver_results.problem.upper_bound - solver_results.problem.lower_bound
-            ) / abs(solver_results.problem.upper_bound)
+            gap = abs(upper_bound - lower_bound) / abs(upper_bound)
         elif solver_results.problem.lower_bound == 0.0:
-            self.gap = 0.0
+            gap = 0.0
         else:
-            self.gap = float("inf")
+            gap = float("inf")
+        self.value("gap", gap)
 
-        if not solver_results.solver.termination_condition == TerminationCondition.optimal:
+        if not solver_results_dict["termination_condition"] == TerminationCondition.optimal:
             self._n_non_optimal_solves += 1
 
-    def _update_metric(self, metric_name, value):
-        data = list(getattr(self, metric_name))
-        data.append(value)
-        setattr(self, "_" + metric_name, tuple(data))
+    def value(self, metric_name: str, set_value=None):
+        if set_value is not None:
+            data = list(self.__getattribute__(f"_{metric_name}"))
+            data.append(set_value)
+            self.__setattr__(f"_{metric_name}", tuple(data))
 
-    @property
-    def start_time(self) -> tuple:
-        return self._start_time
-
-    @start_time.setter
-    def start_time(self, start_hour: int):
-        self._update_metric("start_time", start_hour)
-
-    @property
-    def n_days(self) -> tuple:
-        return self._n_days
-
-    @n_days.setter
-    def n_days(self, solve_days: int):
-        self._update_metric("n_days", solve_days)
-
-    @property
-    def termination_condition(self) -> tuple:
-        return self._termination_condition
-
-    @termination_condition.setter
-    def termination_condition(self, condition: str):
-        self._update_metric("termination_condition", condition)
-
-    @property
-    def solve_time(self) -> tuple:
-        return self._solve_time
-
-    @solve_time.setter
-    def solve_time(self, time: float):
-        self._update_metric("solve_time", time)
-
-    @property
-    def objective(self) -> tuple:
-        return self._objective
-
-    @objective.setter
-    def objective(self, objective_value: float):
-        self._update_metric("objective", objective_value)
-
-    @property
-    def upper_bound(self) -> tuple:
-        return self._upper_bound
-
-    @upper_bound.setter
-    def upper_bound(self, bound: float):
-        self._update_metric("upper_bound", bound)
-
-    @property
-    def lower_bound(self) -> tuple:
-        return self._lower_bound
-
-    @lower_bound.setter
-    def lower_bound(self, bound: float):
-        self._update_metric("lower_bound", bound)
-
-    @property
-    def constraints(self) -> tuple:
-        return self._constraints
-
-    @constraints.setter
-    def constraints(self, constraint_count: int):
-        self._update_metric("constraints", constraint_count)
-
-    @property
-    def variables(self) -> tuple:
-        return self._variables
-
-    @variables.setter
-    def variables(self, variable_count: int):
-        self._update_metric("variables", variable_count)
-
-    @property
-    def non_zeros(self) -> tuple:
-        return self._non_zeros
-
-    @non_zeros.setter
-    def non_zeros(self, non_zeros_count: int):
-        self._update_metric("non_zeros", non_zeros_count)
-
-    @property
-    def gap(self) -> tuple:
-        return self._gap
-
-    @gap.setter
-    def gap(self, mip_gap: int):
-        self._update_metric("gap", mip_gap)
-
-    @property
-    def n_non_optimal_solves(self) -> int:
-        return self._n_non_optimal_solves
+        else:
+            return self.__getattribute__(f"_{metric_name}")
