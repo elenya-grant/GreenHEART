62287e0187f459f137753a3260e7878693026206
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-03-03 13:40:30 -0700

    reverted change to ex 1 run file

--- examples/01_onshore_steel_mn/run_onshore_steel_mn.py
+++ examples/01_onshore_steel_mn/run_onshore_steel_mn.py
@@ -1,12 +1,7 @@
-import os
-from pathlib import Path
-
 import numpy as np
 
 from h2integrate.core.h2integrate_model import H2IntegrateModel
 
-
-os.chdir(Path(__file__).parent)
 
 # Create a H2Integrate model
 model = H2IntegrateModel("01_onshore_steel_mn.yaml")
