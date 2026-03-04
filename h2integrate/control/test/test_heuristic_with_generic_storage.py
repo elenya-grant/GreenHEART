8567676647c2b3e0a32d2e419ad0356733917a66
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-03-04 15:29:44 -0700

    updated tests for generic converter

--- h2integrate/control/test/test_heuristic_with_generic_storage.py
+++ h2integrate/control/test/test_heuristic_with_generic_storage.py
@@ -23,7 +23,7 @@ def plant_config():
             },
         },
         "tech_to_dispatch_connections": [
-            ["battery", "battery"],
+            ["h2_storage", "h2_storage"],
         ],
     }
     return plant_config
@@ -33,38 +33,31 @@ def plant_config():
 def tech_config_generic():
     tech_config = {
         "technologies": {
-            "battery": {
+            "h2_storage": {
                 "dispatch_rule_set": {"model": "PyomoRuleStorageBaseclass"},
                 "control_strategy": {"model": "HeuristicLoadFollowingController"},
                 "performance_model": {"model": "StoragePerformanceModel"},
                 "model_inputs": {
                     "shared_parameters": {
-                        "max_charge_rate": 50000,
-                        "max_capacity": 200000,
+                        "max_charge_rate": 10.0,
+                        "max_capacity": 40.0,
                         "n_control_window": 24,
-                        "n_horizon_window": 48,
-                        "init_charge_percent": 0.5,
-                        "max_charge_percent": 0.9,
+                        "init_charge_percent": 0.1,
+                        "max_charge_percent": 1.0,
                         "min_charge_percent": 0.1,
-                        "commodity": "electricity",
-                        "commodity_rate_units": "kW",
+                        "commodity": "hydrogen",
+                        "commodity_rate_units": "kg/h",
                     },
                     "performance_parameters": {
                         "charge_equals_discharge": True,
-                        "commodity_amount_units": "kW*h",
+                        "commodity_amount_units": "kg",
                         "charge_efficiency": 1.0,
                         "discharge_efficiency": 1.0,
                     },
                     "control_parameters": {
-                        # "commodity": "electricity",
-                        # "commodity_rate_units": "kW",
-                        "tech_name": "battery",
-                        "system_commodity_interface_limit": 1e12,
+                        "tech_name": "h2_storage",
+                        "system_commodity_interface_limit": 10.0,
                     },
-                    # "dispatch_rule_parameters": {
-                    #     "commodity": "electricity",
-                    #     "commodity_rate_units": "kW",
-                    # },
                 },
             }
         },
@@ -74,16 +67,8 @@ def tech_config_generic():
 
 @pytest.mark.regression
 def test_heuristic_load_following_generic_dispatch(plant_config, tech_config_generic, subtests):
-    # Fabricate some oscillating power generation data: 0 kW for the first 12 hours, 10000 kW for
-    # the second twelve hours, and repeat that daily cycle over a year.
-    n_look_ahead_half = int(24 / 2)
-
-    electricity_in = np.concatenate(
-        (np.ones(n_look_ahead_half) * 0, np.ones(n_look_ahead_half) * 10000)
-    )
-    electricity_in = np.tile(electricity_in, 365)
-
-    demand_in = np.ones(8760) * 6000.0
+    commodity_demand = np.full(8760, 5.0)
+    commodity_in = np.tile(np.concat([np.zeros(3), np.cumsum(np.ones(15)), np.full(6, 4.0)]), 365)
 
     # Setup the OpenMDAO problem and add subsystems
     prob = om.Problem()
@@ -91,7 +76,7 @@ def test_heuristic_load_following_generic_dispatch(plant_config, tech_config_gen
     prob.model.add_subsystem(
         "PyomoRuleStorageBaseclass",
         PyomoRuleStorageBaseclass(
-            plant_config=plant_config, tech_config=tech_config_generic["technologies"]["battery"]
+            plant_config=plant_config, tech_config=tech_config_generic["technologies"]["h2_storage"]
         ),
         promotes=["*"],
     )
@@ -99,313 +84,120 @@ def test_heuristic_load_following_generic_dispatch(plant_config, tech_config_gen
     prob.model.add_subsystem(
         "battery_heuristic_load_following_controller",
         HeuristicLoadFollowingController(
-            plant_config=plant_config, tech_config=tech_config_generic["technologies"]["battery"]
+            plant_config=plant_config, tech_config=tech_config_generic["technologies"]["h2_storage"]
         ),
         promotes=["*"],
     )
 
     prob.model.add_subsystem(
-        "battery",
+        "storage",
         StoragePerformanceModel(
-            plant_config=plant_config, tech_config=tech_config_generic["technologies"]["battery"]
+            plant_config=plant_config, tech_config=tech_config_generic["technologies"]["h2_storage"]
         ),
         promotes=["*"],
     )
 
     # Setup the system and required values
     prob.setup()
-    prob.set_val("battery.electricity_in", electricity_in)
-    prob.set_val("battery.electricity_demand", demand_in)
+    prob.set_val("storage.hydrogen_in", commodity_in)
+    prob.set_val("storage.hydrogen_demand", commodity_demand)
 
     # Run the model
     prob.run_model()
 
-    # Test the case where the charging/discharging cycle remains within the max and min SOC limits
-    # Check the expected outputs to actual outputs
-    expected_electricity_out = [
-        5999.99995059,
-        5990.56676743,
-        5990.138959,
-        5989.64831176,
-        5989.08548217,
-        5988.44193888,
-        5987.70577962,
-        5986.86071125,
-        5985.88493352,
-        5984.7496388,
-        5983.41717191,
-        5981.839478,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-        6000.0,
-    ]
+    charge_rate = prob.get_val("storage.max_charge_rate", units="kg/h")[0]
+    discharge_rate = prob.get_val("storage.max_charge_rate", units="kg/h")[0]
+    capacity = prob.get_val("storage.storage_capacity", units="kg")[0]
 
-    expected_battery_electricity_discharge = [
-        5999.99995059,
-        5990.56676743,
-        5990.138959,
-        5989.64831176,
-        5989.08548217,
-        5988.44193888,
-        5987.70577962,
-        5986.86071125,
-        5985.88493352,
-        5984.7496388,
-        5983.41717191,
-        5981.839478,
-        -3988.62235554,
-        -3989.2357847,
-        -3989.76832626,
-        -3990.26170521,
-        -3990.71676106,
-        -3991.13573086,
-        -3991.52143699,
-        -3991.87684905,
-        -3992.20485715,
-        -3992.50815603,
-        -3992.78920148,
-        -3993.05020268,
-    ]
-
-    expected_SOC = [
-        49.39724571,
-        46.54631833,
-        43.69133882,
-        40.83119769,
-        37.96394628,
-        35.08762294,
-        32.20015974,
-        29.29919751,
-        26.38184809,
-        23.44436442,
-        20.48162855,
-        17.48627159,
-        19.47067094,
-        21.44466462,
-        23.40741401,
-        25.36052712,
-        27.30530573,
-        29.24281439,
-        31.17393198,
-        33.09939078,
-        35.01980641,
-        36.93570091,
-        38.84752069,
-        40.75565055,
-    ]
-
-    expected_unmet_demand_out = np.array(
-        [
-            4.93562475e-05,
-            9.43323257e00,
-            9.86104099e00,
-            1.03516883e01,
-            1.09145178e01,
-            1.15580611e01,
-            1.22942204e01,
-            1.31392889e01,
-            1.41150664e01,
-            1.52503612e01,
-            1.65828282e01,
-            1.81605218e01,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-            0.00000000e00,
-        ]
-    )
-
-    expected_unused_commodity_out = np.array(
-        [
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            11.37764445,
-            10.76421514,
-            10.23167373,
-            9.73829458,
-            9.28323883,
-            8.86426912,
-            8.47856327,
-            8.12315078,
-            7.79514283,
-            7.49184426,
-            7.21079852,
-            6.94979705,
-        ]
-    )
-
-    with subtests.test("Check electricity_out"):
+    # Test that discharge is always positive
+    with subtests.test("Discharge is always positive"):
+        assert np.all(prob.get_val("storage.storage_hydrogen_discharge", units="kg/h") >= 0)
+    with subtests.test("Charge is always negative"):
+        assert np.all(prob.get_val("storage.storage_hydrogen_charge", units="kg/h") <= 0)
+    with subtests.test("Charge + Discharge == storage_hydrogen_out"):
+        charge_plus_discharge = prob.get_val(
+            "storage.storage_hydrogen_charge", units="kg/h"
+        ) + prob.get_val("storage.storage_hydrogen_discharge", units="kg/h")
+        np.testing.assert_allclose(
+            charge_plus_discharge, prob.get_val("storage_hydrogen_out", units="kg/h"), rtol=1e-6
+        )
+    with subtests.test("Initial SOC is correct"):
         assert (
-            pytest.approx(expected_electricity_out)
-            == prob.get_val("battery.electricity_out", units="kW")[0:24]
+            pytest.approx(prob.model.get_val("storage.SOC", units="unitless")[0], rel=1e-6) == 0.1
         )
 
-    with subtests.test("Check battery_electricity_discharge"):
-        assert (
-            pytest.approx(expected_battery_electricity_discharge)
-            == prob.get_val("battery.battery_electricity_discharge", units="kW")[0:24]
+    indx_soc_increase = np.argwhere(
+        np.diff(prob.model.get_val("storage.SOC", units="unitless"), prepend=True) > 0
+    ).flatten()
+    indx_soc_decrease = np.argwhere(
+        np.diff(prob.model.get_val("storage.SOC", units="unitless"), prepend=False) < 0
+    ).flatten()
+    indx_soc_same = np.argwhere(
+        np.diff(prob.model.get_val("storage.SOC", units="unitless"), prepend=True) == 0.0
+    ).flatten()
+
+    with subtests.test("SOC increases when charging"):
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_charge", units="kg/h")[indx_soc_increase] < 0
+        )
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_charge", units="kg/h")[indx_soc_decrease] == 0
+        )
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_charge", units="kg/h")[indx_soc_same] == 0
         )
 
-    with subtests.test("Check SOC"):
-        assert pytest.approx(expected_SOC) == prob.get_val("battery.SOC", units="percent")[0:24]
-
-    with subtests.test("Check unmet_demand"):
-        assert (
-            pytest.approx(expected_unmet_demand_out, abs=1e-4)
-            == prob.get_val("battery.unmet_electricity_demand_out", units="kW")[0:24]
+    with subtests.test("SOC decreases when discharging"):
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h")[indx_soc_decrease] > 0
+        )
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h")[indx_soc_increase] == 0
+        )
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h")[indx_soc_same] == 0
         )
 
-    with subtests.test("Check unused_electricity_out"):
+    with subtests.test("Max SOC <= Max storage percent"):
+        assert prob.get_val("storage.SOC", units="unitless").max() <= 1.0
+
+    with subtests.test("Min SOC >= Min storage percent"):
+        assert prob.get_val("storage.SOC", units="unitless").min() >= 0.1
+
+    with subtests.test("Charge never exceeds charge rate"):
         assert (
-            pytest.approx(expected_unused_commodity_out)
-            == prob.get_val("battery.unused_electricity_out", units="kW")[0:24]
+            prob.get_val("storage.storage_hydrogen_charge", units="kg/h").min() >= -1 * charge_rate
         )
 
-    # Test the case where the battery is discharged to its lower SOC limit
-    electricity_in = np.zeros(8760)
-    demand_in = np.ones(8760) * 30000
-
-    # Setup the system and required values
-    prob.setup()
-    prob.set_val("battery.electricity_in", electricity_in)
-    prob.set_val("battery.electricity_demand", demand_in)
-
-    # Run the model
-    prob.run_model()
-
-    expected_electricity_out = np.array(
-        [3.00000000e04, 2.99305601e04, 2.48145097e04, 4.97901621e00, 3.04065390e01]
-    )
-    expected_battery_electricity_discharge = expected_electricity_out
-    expected_SOC = np.array([37.69010284, 22.89921133, 10.00249593, 10.01524461, 10.03556385])
-    expected_unmet_demand_out = np.array(
-        [
-            9.43691703e-09,
-            6.94398578e01,
-            5.18549025244965e03,
-            2.999502098378662e04,
-            2.9969593461021406e04,
-        ]
-    )
-    expected_unused_commodity_out = np.zeros(5)
-
-    with subtests.test("Check electricity_out for min SOC"):
+    with subtests.test("Discharge never exceeds discharge rate"):
         assert (
-            pytest.approx(expected_electricity_out)
-            == prob.get_val("battery.electricity_out", units="kW")[:5]
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h").max() <= discharge_rate
         )
 
-    with subtests.test("Check battery_electricity_discharge for min SOC"):
-        assert (
-            pytest.approx(expected_battery_electricity_discharge)
-            == prob.get_val("battery.battery_electricity_discharge", units="kW")[:5]
+    with subtests.test("Discharge never exceeds demand"):
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h").max()
+            <= commodity_demand
         )
 
-    with subtests.test("Check SOC for min SOC"):
-        assert pytest.approx(expected_SOC) == prob.get_val("battery.SOC", units="percent")[:5]
+    with subtests.test("Cumulative charge/discharge does not exceed storage capacity"):
+        assert np.cumsum(prob.get_val("storage_hydrogen_out", units="kg/h")).max() <= capacity
+        assert np.cumsum(prob.get_val("storage_hydrogen_out", units="kg/h")).min() >= -1 * capacity
 
-    with subtests.test("Check unmet_demand for min SOC"):
-        assert (
-            pytest.approx(expected_unmet_demand_out, abs=1e-6)
-            == prob.get_val("battery.unmet_electricity_demand_out", units="kW")[:5]
+    with subtests.test("Expected discharge from hour 10-30"):
+        expected_discharge = np.concat(
+            [np.zeros(8), np.ones(6), np.full(3, 5.0), np.array([4, 3, 2])]
+        )
+        np.testing.assert_allclose(
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h")[10:30],
+            expected_discharge,
+            rtol=1e-6,
         )
 
-    with subtests.test("Check unused_commodity_out for min SOC"):
-        assert (
-            pytest.approx(expected_unused_commodity_out)
-            == prob.get_val("battery.unused_electricity_out", units="kW")[:5]
-        )
-
-    # Test the case where the battery is charged to its upper SOC limit
-    electricity_in = np.ones(8760) * 30000.0
-    demand_in = np.zeros(8760)
-
-    # Setup the system and required values
-    prob.setup()
-    prob.set_val("battery.electricity_in", electricity_in)
-    prob.set_val("battery.electricity_demand", demand_in)
-
-    # Run the model
-    prob.run_model()
-
-    expected_electricity_out = [-0.008477085, 0.0, 0.0, 0.0, 0.0]
-
-    # TODO reevaluate the output here
-    expected_battery_electricity_discharge = np.array(
-        [-30000.00847709, -29973.58679719, -21109.22734423, 0.0, 0.0]
-    )
-
-    # expected_SOC = [66.00200558, 79.43840635, 90.0, 90.0, 90.0]
-    expected_SOC = np.array([66.00200558, 79.43840635, 89.02326413, 89.02326413, 89.02326413])
-    expected_unmet_demand_out = np.array([0.00847709, 0.0, 0.0, 0.0, 0.0])
-    expected_unused_commodity_out = np.array(
-        [0.00000000e00, 2.64132028e01, 8.89077266e03, 3.04088135e04, 3.00564087e04]
-    )
-    # I think this is the right expected_electricity_out since the battery won't
-    # be discharging in this instance
-    # expected_electricity_out = [0.0, 0.0, 0.0, 0.0, 0.0]
-    # # expected_electricity_out = [0.0, 0.0, 6150.14483911, 30000.0, 30000.0]
-    # expected_battery_electricity_discharge = [-30000.00847705, -29973.58679681,
-    # -23310.54620182, 0.0, 0.0]
-    # expected_SOC = [66.00200558, 79.43840635, 90.0, 90.0, 90.0]
-    # expected_unmet_demand_out = np.zeros(5)
-    # expected_unused_commodity_out = [0.0, 0.0, 6150.14483911, 30000.0, 30000.0]
-
-    abs_tol = 1e-6
-    rel_tol = 1e-1
-    with subtests.test("Check electricity_out for max SOC"):
-        assert (
-            pytest.approx(expected_electricity_out, abs=abs_tol, rel=rel_tol)
-            == prob.get_val("battery.electricity_out", units="kW")[:5]
-        )
-
-    with subtests.test("Check battery_electricity_discharge for max SOC"):
-        assert (
-            pytest.approx(expected_battery_electricity_discharge, abs=abs_tol, rel=rel_tol)
-            == prob.get_val("battery.battery_electricity_discharge", units="kW")[:5]
-        )
-
-    with subtests.test("Check SOC for max SOC"):
-        assert (
-            pytest.approx(expected_SOC, abs=abs_tol)
-            == prob.get_val("battery.SOC", units="percent")[:5]
-        )
-
-    with subtests.test("Check unmet_demand for max SOC"):
-        assert (
-            pytest.approx(expected_unmet_demand_out, abs=abs_tol)
-            == prob.get_val("battery.unmet_electricity_demand_out", units="kW")[:5]
-        )
-
-    with subtests.test("Check unused_commodity_out for max SOC"):
-        assert (
-            pytest.approx(expected_unused_commodity_out, abs=abs_tol, rel=rel_tol)
-            == prob.get_val("battery.unused_electricity_out", units="kW")[:5]
+    with subtests.test("Expected charge hour 0-20"):
+        expected_charge = np.concat([np.zeros(8), np.arange(-1, -9, -1), np.zeros(4)])
+        np.testing.assert_allclose(
+            prob.get_val("storage.storage_hydrogen_charge", units="kg/h")[0:20],
+            expected_charge,
+            rtol=1e-6,
         )
--- h2integrate/storage/test/test_generic_storage_pyo.py
+++ h2integrate/storage/test/test_generic_storage_pyo.py
@@ -7,7 +7,7 @@ from h2integrate.storage.generic_storage_pyo import StoragePerformanceModel
 
 @pytest.mark.regression
 @pytest.mark.parametrize("n_timesteps", [24])
-def test_generic_storage_without_controller(plant_config, subtests):
+def test_generic_storage_without_controller_dmd_lessthan_charge_rate(plant_config, subtests):
     performance_model_config = {
         "commodity": "hydrogen",
         "commodity_rate_units": "kg/h",
@@ -53,11 +53,17 @@ def test_generic_storage_without_controller(plant_config, subtests):
 
     prob.run_model()
 
+    charge_rate = prob.get_val("storage.max_charge_rate", units="kg/h")[0]
+    discharge_rate = prob.get_val("storage.max_charge_rate", units="kg/h")[0]
+    capacity = prob.get_val("storage.storage_capacity", units="kg")[0]
+
     # Test that discharge is always positive
     with subtests.test("Discharge is always positive"):
         assert np.all(prob.get_val("storage.storage_hydrogen_discharge", units="kg/h") >= 0)
+
     with subtests.test("Charge is always negative"):
         assert np.all(prob.get_val("storage.storage_hydrogen_charge", units="kg/h") <= 0)
+
     with subtests.test("Charge + Discharge == storage_hydrogen_out"):
         charge_plus_discharge = prob.get_val(
             "storage.storage_hydrogen_charge", units="kg/h"
@@ -109,14 +115,24 @@ def test_generic_storage_without_controller(plant_config, subtests):
         assert prob.get_val("storage.SOC", units="unitless").min() >= 0.1
 
     with subtests.test("Charge never exceeds charge rate"):
-        assert prob.get_val("storage.storage_hydrogen_charge", units="kg/h").min() >= -10
+        assert (
+            prob.get_val("storage.storage_hydrogen_charge", units="kg/h").min() >= -1 * charge_rate
+        )
 
     with subtests.test("Discharge never exceeds discharge rate"):
-        assert prob.get_val("storage.storage_hydrogen_discharge", units="kg/h").max() <= 10.0
+        assert (
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h").max() <= discharge_rate
+        )
+
+    with subtests.test("Discharge never exceeds demand"):
+        assert np.all(
+            prob.get_val("storage.storage_hydrogen_discharge", units="kg/h").max()
+            <= commodity_demand
+        )
 
     with subtests.test("Cumulative charge/discharge does not exceed storage capacity"):
-        assert np.cumsum(prob.get_val("storage_hydrogen_out", units="kg/h")).max() <= 40.0
-        assert np.cumsum(prob.get_val("storage_hydrogen_out", units="kg/h")).min() >= -40.0
+        assert np.cumsum(prob.get_val("storage_hydrogen_out", units="kg/h")).max() <= capacity
+        assert np.cumsum(prob.get_val("storage_hydrogen_out", units="kg/h")).min() >= -1 * capacity
 
     with subtests.test("Expected discharge"):
         expected_discharge = np.concat([np.zeros(18), np.ones(6)])
