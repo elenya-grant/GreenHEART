2d6b0c5192b4dd118252acf4894164a9a6145445
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-04-06 10:54:23 -0600

    moved converter control strategies to demand folder

--- h2integrate/demand/demand_openloop_converter_controller.py
+++ h2integrate/demand/demand_openloop_converter_controller.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from h2integrate.control.control_strategies.converters.openloop_controller_base import (
+from h2integrate.demand.openloop_controller_base import (
     ConverterOpenLoopControlBase,
     ConverterOpenLoopControlBaseConfig,
 )
