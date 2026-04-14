15e757306e28cfb1f584162666c88ccf4a18263d
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-04-14 16:13:17 -0600

    minor update to example tests

--- examples/test/test_all_examples.py
+++ examples/test/test_all_examples.py
@@ -803,11 +803,11 @@ def test_electrolyzer_demand(subtests, temp_copy_of_example):
 
     with subtests.test("Electrolyzer capacity factor (Year 0) (battery for min power)"):
         elec_cf_yr0 = h2i.prob.get_val("electrolyzer.capacity_factor", units="percent")[0]
-        assert pytest.approx(38.64543851, rel=1e-6) == elec_cf_yr0
+        assert pytest.approx(38.64543851, rel=1e-4) == elec_cf_yr0
 
     with subtests.test("Electrical load capacity factor (battery for min power)"):
         load_cf = h2i.prob.get_val("elec_load_demand.capacity_factor", units="percent")[0]
-        assert pytest.approx(37.45560830, rel=1e-4) == load_cf
+        assert pytest.approx(37.45560830, rel=1e-6) == load_cf
 
     with subtests.test("Electricity to electrolyzer (battery for min power)"):
         electricity_to_electrolyzer = h2i.prob.get_val("electrolyzer.electricity_in", "MW").sum()
